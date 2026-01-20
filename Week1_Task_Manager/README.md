f535a7ea3186006ba0a38bd92648f7c340a87ac6
Saattaja <markku.putaala@icloud.com>
2026-01-20 13:49:04 +0200

    Week2 done

--- Week1_Task_Manager/README.md
+++ Week1_Task_Manager/README.md
@@ -1,1 +1,8 @@
 # Head to app folder!
+
+MobileAppDevelopment/
+README.md  ← Main README (explains the whole repository)
+Week1_Task_Manager/
+ ->README.md  ← Week 1 specific README
+app/build.
+
---
+++ Week2_ViewModel/app/src/main/java/com/example/week2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.week2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.week2.ui.theme.Viikko1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Viikko1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
