66322c9a1b5ff984adb179faad117f1cd259d2a2
Saattaja <markku.putaala@icloud.com>
2026-01-28 10:59:41 +0200

    Week3 assignment done

---
+++ Week3_MVVM/app/src/main/java/com/example/week3/viewmodel/TaskViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.week3.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.week3.domain.mockTasks
+import com.example.week3.model.Task
+
+
+class TaskViewModel : ViewModel() {
+
+    var tasks by mutableStateOf<List<Task>>(listOf())
+        private set
+
+    private var allTasks: List<Task> = listOf()
+
+    init {
+        allTasks = mockTasks
+        tasks = mockTasks
+    }
+
+    fun addTask(task: Task) {
+        tasks = tasks + task
+        allTasks = tasks
+    }
+
+    fun toggleDone(id: Int) {
+        tasks = tasks.map { if (it.id == id) it.copy(done = !it.done) else it }
+        allTasks = allTasks.map { if (it.id == id) it.copy(done = !it.done) else it }
+    }
+
+    fun updateTask(updatedTask: Task) {
+        tasks = tasks.map { if (it.id == updatedTask.id) updatedTask else it }
+        allTasks = allTasks.map { if (it.id == updatedTask.id) updatedTask else it }
+    }
+
+    fun removeTask(id: Int) {
+        tasks = tasks.filter { it.id != id }
+        allTasks = allTasks.filter { it.id != id }
+    }
+
+    fun filterByDone(done: Boolean) {
+        tasks = allTasks.filter { it.done == done }
+    }
+
+    fun sortByDueDate() {
+        tasks = tasks.sortedBy { it.dueDate } // assumes dueDate is "YYYY-MM-DD"
+    }
+
+    fun showAllTasks() {
+        tasks = allTasks
+    }
+}
